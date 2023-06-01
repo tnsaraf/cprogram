1546696721b8a54502146f23b0bb6f53e3e1d377
Tejas Saraf <tejasnitinsaraf@gmail.com>
2023-06-01 17:15:14 +0530

    Create cprogram2.c adding this file

---
+++ cprogram2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>  
+int main()  
+{  
+   float roi;  
+   roi = 10.00;  
+   //it will print 6 digit decimal number after the decimal points.  
+   printf("The rate of interest for the investment is %f \n", roi);  
+   return 0;  
+}  
